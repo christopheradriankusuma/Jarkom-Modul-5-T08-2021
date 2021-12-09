4e641dec8b7f037f9d43c08cde8aad38401fb1dc
Christopher Adrian Kusuma <32239110+christopheradriankusuma@users.noreply.github.com>
2021-12-09 18:57:49 +0700

    Update iptables.sh

--- Foosha/iptables.sh
+++ Foosha/iptables.sh
@@ -1,12 +1,13 @@
 # internet
-iptables -t nat -A POSTROUTING -s 192.215.0.8/29 -j SNAT --to-source 192.168.122.97
-iptables -t nat -A POSTROUTING -s 192.215.0.0/30 -j SNAT --to-source 192.168.122.97
-iptables -t nat -A POSTROUTING -s 192.215.4.0/22 -j SNAT --to-source 192.168.122.97
-iptables -t nat -A POSTROUTING -s 192.215.0.128/25 -j SNAT --to-source 192.168.122.97
-iptables -t nat -A POSTROUTING -s 192.215.0.4/30 -j SNAT --to-source 192.168.122.97
-iptables -t nat -A POSTROUTING -s 192.215.2.0/23 -j SNAT --to-source 192.168.122.97
-iptables -t nat -A POSTROUTING -s 192.215.1.0/24 -j SNAT --to-source 192.168.122.97
-iptables -t nat -A POSTROUTING -s 192.215.0.16/29 -j SNAT --to-source 192.168.122.97
+IP=`ip -br a s eth0 | awk '{print $3}' | awk -F '/' '{print $1}'`
+iptables -t nat -A POSTROUTING -s 192.215.0.8/29 -j SNAT --to-source $IP
+iptables -t nat -A POSTROUTING -s 192.215.0.0/30 -j SNAT --to-source $IP
+iptables -t nat -A POSTROUTING -s 192.215.4.0/22 -j SNAT --to-source $IP
+iptables -t nat -A POSTROUTING -s 192.215.0.128/25 -j SNAT --to-source $IP
+iptables -t nat -A POSTROUTING -s 192.215.0.4/30 -j SNAT --to-source $IP
+iptables -t nat -A POSTROUTING -s 192.215.2.0/23 -j SNAT --to-source $IP
+iptables -t nat -A POSTROUTING -s 192.215.1.0/24 -j SNAT --to-source $IP
+iptables -t nat -A POSTROUTING -s 192.215.0.16/29 -j SNAT --to-source $IP
 
 iptables -A FORWARD -p tcp --dport 80 -j DROP ! -s 192.215.0.0/21 -d 192.215.0.8/29
 
